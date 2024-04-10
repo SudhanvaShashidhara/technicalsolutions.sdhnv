6049c3bc2fbed2b71c1e605866f2f1cd62fbf14f
Sudhanva Shashidhara <sudhanvashashidhara@Sudhanvas-MacBook-Air.local>
2024-04-10 17:35:02 +0530

    Added EC hased codes on backend

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -7,11 +7,11 @@
             return res.json();
         })
         .then(function(data){
-            // window.gtag('set', 'user_data', {
-            //     "sha256_email_address": data.email_hash
-            // });
-
-            window.dataLayer.push({'sha256_email_address': data.email_hash, 'event': 'contact_form_submit', 'email_hash2': data.email_hash2});
+            window.gtag('set', 'user_data', {
+                "sha256_email_address": data.email_hash
+            });
+            window.gtag('event', 'conversion', {'send_to': 'AW-10852928527/FWmCCJe6oqUZEI-Yirco'});
+            // window.dataLayer.push({'sha256_email_address': data.email_hash, 'event': 'contact_form_submit', 'email_hash2': data.email_hash2});
         });
     }
 </script>
