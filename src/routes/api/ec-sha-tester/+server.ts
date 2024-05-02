fa7719e5fb78ddf1c585eeb93dd4f56cdd2b3df4
Sudhanva Shashidhara <sudhanvashashidhara@Sudhanvas-MacBook-Air.local>
2024-05-02 23:32:35 +0530

    EC Sha Tester for non gmail

--- src/routes/api/ec-sha-tester/+server.ts
+++ src/routes/api/ec-sha-tester/+server.ts
@@ -1,55 +1,51 @@
 import { json } from '@sveltejs/kit';
 import crypto from "crypto";
-// import { initializeApp, cert, getApps } from "firebase-admin/app";
-// import { getFirestore } from "firebase-admin/firestore";
+import { initializeApp, cert, getApps } from "firebase-admin/app";
+import { getFirestore, FieldValue } from "firebase-admin/firestore";
 
-// import { GSA_TYPE, GSA_PROJECT_ID, GSA_PRIVATE_KEY_ID, GSA_PRIVATE_KEY, GSA_CLIENT_EMAIL, GSA_CLIENT_ID, GSA_AUTH_URI, GSA_TOKEN_URI, GSA_AUTH_PROVIDER_X509_CERT_URL, GSA_CLIENT_X509_CERT_URL, GSA_UNIVERSE_DOMAIN } from "$env/static/private";
+import { GSA_TYPE, GSA_PROJECT_ID, GSA_PRIVATE_KEY_ID, GSA_PRIVATE_KEY, GSA_CLIENT_EMAIL, GSA_CLIENT_ID, GSA_AUTH_URI, GSA_TOKEN_URI, GSA_AUTH_PROVIDER_X509_CERT_URL, GSA_CLIENT_X509_CERT_URL, GSA_UNIVERSE_DOMAIN } from "$env/static/private";
 
 import type { RequestHandler } from './$types';
 
-// const GSA_PRIVATE_KEY_FINAL = GSA_PRIVATE_KEY.replace(/\\n/g, '\n');
+const GSA_PRIVATE_KEY_FINAL = GSA_PRIVATE_KEY.replace(/\\n/g, '\n');
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-// const firebase_service_account : any = {
-// 	"type": GSA_TYPE,
-// 	"project_id": GSA_PROJECT_ID,
-// 	"private_key_id": GSA_PRIVATE_KEY_ID,
-// 	"private_key": GSA_PRIVATE_KEY_FINAL,
-// 	"client_email": GSA_CLIENT_EMAIL,
-// 	"client_id": GSA_CLIENT_ID,
-// 	"auth_uri": GSA_AUTH_URI,
-// 	"token_uri": GSA_TOKEN_URI,
-// 	"auth_provider_x509_cert_url": GSA_AUTH_PROVIDER_X509_CERT_URL,
-// 	"client_x509_cert_url": GSA_CLIENT_X509_CERT_URL,
-// 	"universe_domain": GSA_UNIVERSE_DOMAIN
-// };
+//eslint-disable-next-line @typescript-eslint/no-explicit-any
+const firebase_service_account : any = {
+	"type": GSA_TYPE,
+	"project_id": GSA_PROJECT_ID,
+	"private_key_id": GSA_PRIVATE_KEY_ID,
+	"private_key": GSA_PRIVATE_KEY_FINAL,
+	"client_email": GSA_CLIENT_EMAIL,
+	"client_id": GSA_CLIENT_ID,
+	"auth_uri": GSA_AUTH_URI,
+	"token_uri": GSA_TOKEN_URI,
+	"auth_provider_x509_cert_url": GSA_AUTH_PROVIDER_X509_CERT_URL,
+	"client_x509_cert_url": GSA_CLIENT_X509_CERT_URL,
+	"universe_domain": GSA_UNIVERSE_DOMAIN
+};
 
-// if(!getApps().length){
-// 	initializeApp({
-// 		credential: cert(firebase_service_account)
-// 	});  
-// }
-// else{
-// 	getApps()[0]
-// }
+if(!getApps().length){
+	initializeApp({
+		credential: cert(firebase_service_account)
+	});  
+}
+else{
+	getApps()[0]
+}
 
-// const db = getFirestore();
-// db.settings({ ignoreUndefinedProperties: true });
+const db = getFirestore();
+db.settings({ ignoreUndefinedProperties: true });
 
-export const GET: RequestHandler = async ({  url }) => {	
+export const GET: RequestHandler = async ({ url, getClientAddress }) => {	
 
 	const email = url.searchParams.get('email');
+  const ip_address = getClientAddress();
 
     if (email) {
         const email_parts = email?.split("@");
-        const is_gmail = /(gmail|googlemail)\.com/g.test(email_parts[1]);
         let normalized_email = "", normalized_email2 = "";
 
-        if (!is_gmail) {
-          return json({ error: "E Mail needs to be gmail or googlemail" });
-        }
-
-        if (is_gmail && email_parts.length > 1) {
+        if (email_parts.length > 1) {
 
           normalized_email = email_parts[0].replace(".", "").toLowerCase() + "@" + email_parts[1].toLowerCase();
           normalized_email2 = email.trim().toLowerCase();
@@ -74,6 +70,12 @@ export const GET: RequestHandler = async ({  url }) => {
             .createHash("sha256")
             // updating data
             .update(normalized_email2);
+
+            try{
+              await db.collection('ec-sha-tester').add({ ip_address, timestamp: FieldValue.serverTimestamp(), email_hash, email_hash2, email_hash3, email_hash4 });
+            }catch(err){
+              console.log(err);
+            }	
 
           return json({ email_hash, email_hash2, email_hash3, email_hash4 });
         }
